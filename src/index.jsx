46d31da0e8494247311808430bc518e780a0d10b
psyhedeliq <filipcornelcristian@gmail.com>
2020-02-11 01:13:57 +0000

    feat: [psyhedeliq] create first component this closes #5

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './component/app';
+
 const renderApp = document.querySelector('#render-app');
-// console.log(renderApp);
+
+render(<App />, renderApp);
