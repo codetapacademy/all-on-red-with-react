7f3e5a36c7e8110126cfa539695ef87b97371766
biancalina <bianca.alyna30@gmail.com>
2020-02-13 12:10:44 +0000

    create my first component

---
+++ src/component/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './component/app'
+
+const hereRendersMyApp = document.querySelector('#here-renders-my-app')
+
+render (
+  <App/>,
+  hereRedndersMyApp
+)
