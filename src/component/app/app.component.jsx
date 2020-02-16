26bd11283d462a43e5cde874f4e50e84200f9e47
Robert Alexandru <romaniarobertverde@yahoo.com>
2020-02-16 09:08:37 +0000

    Create the circle board component

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import CircleBoard from '../circle-board'
 
 const App = () => {
   return (
-    <h1>App</h1>
+    <div>
+      <h1>App</h1>
+      <CircleBoard />
+    </div>
   )
 }
 
---
+++ src/component/circle-board/circle-board.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CircleBoard = () => {
+  return (
+    <p>This will be the board</p>
+  )
+}
+
+export default CircleBoard
