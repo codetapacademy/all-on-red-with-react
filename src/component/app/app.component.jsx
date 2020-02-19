1bbca9bb7c35189e731dfbb2dbb4f240a7a9d5c7
Bogdan Negura <negura.bogdan.90@gmail.com>
2020-02-19 17:12:11 +0000

    first setUp and create the App first component

---
+++ src/component/app/app.component.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <div>Salut</div>;
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+
+import App from "./component/app";
+// import App from "./component/app/app.component";
+
+const reactWillRenderHere = document.querySelector("#react-will-render-here");
+
+render(<App />, reactWillRenderHere);
