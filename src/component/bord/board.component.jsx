ef396d319c071856e93e9fcf5f06ff44927db478
Bogdan Negura <negura.bogdan.90@gmail.com>
2020-02-22 20:59:09 +0000

    feat: [bogdannegura] - generate a slot list as a collection of objects with numbers

--- src/component/bord/board.component.jsx
+++ src/component/bord/board.component.jsx
@@ -3,6 +3,31 @@ import { StyledBoard } from "./board.style";
 import Slot from "../slot";
 
 const Board = () => {
+  const length = 18;
+  const slotList = [
+    {
+      number: "0",
+      rotation: 0,
+      color: "green"
+    },
+    ...Array.from({ length }, (_, key) => key + 1).map(n => ({
+      number: n + "",
+      rotation: ~~((360 / 38) * n * 100) / 100,
+      color: n % 2 ? "black" : "red"
+    })),
+    {
+      number: "00",
+      rotation: 180,
+      color: "green"
+    },
+    ...Array.from({ length }, (_, key) => key + 19).map(n => ({
+      number: n + "",
+      rotation: ~~((360 / 38) * (n + 1) * 100) / 100,
+      color: n % 2 ? "red" : "black"
+    }))
+  ];
+  console.log(slotList);
+
   return (
     <StyledBoard side={350}>
       <Slot />
